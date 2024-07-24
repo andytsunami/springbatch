1fe3928362cbce61fb35e1c022e6252a231dc6c1
André Vasconcelos <gp-andre.vasconcelos@uol.com.br>
2024-07-24 19:19:02 -0300

    01 - Primeiro commit com Ola Mundo

---
+++ primeiroprojetospringbatch/src/main/java/com/udemy/primeiroprojetospringbatch/PrimeiroprojetospringbatchApplication.java
@@ -0,0 +1,12 @@
+package com.udemy.primeiroprojetospringbatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrimeiroprojetospringbatchApplication {
+    public static void main(String[] args) {
+        System.out.println("Começando...");
+        SpringApplication.run(PrimeiroprojetospringbatchApplication.class,args);
+    }
+}
